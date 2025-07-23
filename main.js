129e24f2d34d4295befd5f66f9d10b93e2f48b6c
urmee04 <urmee04sust@gmail.com>
2025-07-23 07:25:03 -0400

    Add DOM element references and setup for URL shortener interface

---
+++ main.js
@@ -0,0 +1,7 @@
+// DOMContentLoaded event to ensure all HTML is loaded before running script
+document.addEventListener("DOMContentLoaded", function () {
+  const urlInput = document.getElementById("url-input");
+  const errMessage = document.getElementById("error-message");
+  const activateBtn = document.getElementById("activate-btn");
+  const linksHistory = document.getElementById("link-history");
+});
